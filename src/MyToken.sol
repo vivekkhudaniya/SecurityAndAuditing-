cd55110fd342bd1f3126ef66a55a462f26872103
vivekkhudaniya <vivekkhudaniya@gmail.com>
2024-01-06 03:40:12 +0530

    encoding

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,9 +1,9 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-// import {ERC20} from "@openzeppelin/contracts/token/ERC2O/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC2O/ERC20.sol";
 
-// contract MyToken is ERC20{
+contract MyToken is ERC20{
     
-//     constructor() ERC20("MyTokenName","MTN"){}
-// }
+    constructor() ERC20("MyTokenName","MTN"){}
+}
